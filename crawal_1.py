993b005df7f9d75a0d171c6ff144f6169ca13766
xjtugzq <50620228+xjtugzq@users.noreply.github.com>
2019-10-27 23:38:52 +0800

    Revert "Revert "爬虫的第一次尝试，卑微的代码搬运工。。。""
    This reverts commit 3e603f314a20b69ef65d8f1af4dbf612fe032b48.

---
+++ crawal_1.py
@@ -0,0 +1,20 @@
+'''
+获取简书首页文章标题（抄的）
+'''
+from urllib import request
+from bs4 import BeautifulSoup as bs
+url = r'http://www.jianshu.com'
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.87 Safari/537.36'}
+page = request.Request(url, headers=headers)
+page_info = request.urlopen(page).read().decode('utf-8')
+soup = bs(page_info, 'html.parser')
+titles = soup.find_all('a', 'title')
+#for title in titles:
+#    print(title.string)
+try:
+    file = open(r'H:\code\Python\title.txt', 'w')
+    for title in titles:
+        file.write(title.string + '\n')
+finally:
+    if file:
+        file.close()
